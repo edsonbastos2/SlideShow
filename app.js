ab07b283597d586f3e175abeaa364c60c12a1a93
edsonbastos2 <edsonbastos2@hotmail.com>
2019-10-15 22:41:53 -0300

    Teste consumir API
    Primeiro teste tentando consumir uma API, de um JSON

--- app.js
+++ app.js
@@ -30,3 +30,19 @@ function botoes(n, m){
         }
     }
 }
+
+/*Consumir API rest*/
+const url = 'https://dt11.pepblast.com/pzAlbums_php03/PSO/PSOApi.php?client=HTML&app=PLUGIN&m=pso.remix.getRemixPars&args={"rid":"x85in000149b85wn0lamsj","av":"v1","esort":0,"easc":0,"getappelements":1,"getinputusers":1,"getpropsjs":1,"getisfollower":1,"usecreatoresort":1,"enmax":120}';
+//const url = 'https://jsonplaceholder.typicode.com/users'
+fetch(url)
+    .then( (response) => response.json())
+    .then(function(data){ 
+        console.log(data)
+        
+        return data.map(function(obj){
+            let li = document.createElement('li');
+            li.innerHTML = `${obj} ${obj}`
+            ul.appendChild(li)
+        })
+    })
+    .catch(err => console.log(err))
